819dbece898eab7a26729ee4180d18399b700605
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-09-19 11:57:44 +0200

    6.2 What is Test Driven Development (TDD)?

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculaor/Calculator.java
@@ -0,0 +1,25 @@
+package com.kodilla.testing.calculaor;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public Calculator(int a, int b) {
+    }
+
+    public int addAToB(){
+        return a + b;
+    }
+
+    public int subtractBFromA(){
+        return a - b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
