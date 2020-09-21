c5aa434accfb267d39e90dc2f6c2018f3f3dff77
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-09-21 22:34:26 +0200

    6.3 JUnit, first tests!

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,34 +5,6 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-        //Here will be next test->
-        System.out.println("Test - pierwszy test jednostkowy");
-
-        Calculator calculator = new Calculator(45, 15);
-
-        int sum = calculator.addAToB();
-
-        if (sum == calculator.getA() + calculator.getB()){
-            System.out.println("Adding test OK");
-        } else {
-            System.out.println("Adding error!");
-        }
-
-        int subtract = calculator.subtractBFromA();
-
-        if (subtract == calculator.getA() - calculator.getB()){
-            System.out.println("Subtraction test OK");
-        } else {
-            System.out.println("Subtraction error!");
-        }
     }
 }
