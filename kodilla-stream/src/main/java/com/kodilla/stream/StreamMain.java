4855d95d7edbd3f5095c7f1cab655e6c230bb71c
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-10-03 19:28:58 +0200

    7.3 Knowing the functions stram(), filter(), map() and collect() - commit 1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,110 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args){
+        Forum theForum = new Forum();
+        Map<Integer, ForumUser> theResultMapOfForumUsers = theForum.getUsersList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getBirthDate().getYear() < 2000)
+                .filter(forumUser -> forumUser.getPostCount() > 0)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
+        System.out.println("# elements: " + theResultMapOfForumUsers.size());
+        theResultMapOfForumUsers.entrySet().stream()
+                .map(entry -> "ID " + entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+    }
+}
+
+/*
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args){
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
+
+        System.out.println(theResultStringOfBooks);
+    }
+}
+ */
+/*
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args){
+        BookDirectory theBookDirectory = new BookDirectory();
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+    }
+}
+ */
+
+/*
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args){
+        BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                //.forEach(System.out::println);
+                .collect(Collectors.toList());
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+    }
+}
+ */
+
+/*
+import com.kodilla.stream.person.People;
+
+public class StreamMain {
+    public static void main(String[] args){
+        People.getList().stream().forEach(System.out::println);
+        People.getList().stream().map(s -> s.toUpperCase()).forEach(System.out::println);
+        People.getList().stream().map(String::toUpperCase).forEach(s -> System.out.println(s));
+        People.getList().stream().filter(s -> s.length() > 11).forEach(System.out::println);
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+    }
+}
+ */
+
+
+
+/*
 import com.kodilla.stream.baeutifier.PoemBeautifier;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
@@ -54,3 +159,4 @@ public class StreamMain {
         NumbersGenerator.generateEven(20);
     }
 }
+ */
