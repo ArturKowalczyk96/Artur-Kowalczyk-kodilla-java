9f7412d69cd013f20e02c7153b53fde6841df76e
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2021-02-01 21:50:45 +0100

    15.1 Design patterns part 1

--- kodilla-patterns/src/main/java/com/kodilla/patterns/enums/hotel/PriceList.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/enums/hotel/PriceList.java
@@ -4,6 +4,30 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 public class PriceList {
+
+    private static Map<Season, BigDecimal> singleBedPrices = Map.of(
+            Season.LOW, new BigDecimal(250),
+            Season.HIGH, new BigDecimal(320),
+            Season.HOLIDAY, new BigDecimal(400)
+    );
+
+    private static Map<Season, BigDecimal> doubleBedPrices = Map.of(
+            Season.LOW, new BigDecimal(350),
+            Season.HIGH, new BigDecimal(400),
+            Season.HOLIDAY, new BigDecimal(500)
+    );
+
+    public static BigDecimal getSingleBedPrice(Season season) {
+        return singleBedPrices.get(season);
+    }
+
+    public static BigDecimal getDoubleBedPrice(Season season) {
+        return doubleBedPrices.get(season);
+    }
+
+
+    /*
+    //1
     private static Map<String, BigDecimal> singleBedPrices = Map.of(
             "LOW", new BigDecimal(250),
             "HIGH", new BigDecimal(320),
@@ -22,6 +46,6 @@ public class PriceList {
 
     public static BigDecimal getDoubleBedPrice(String season) {
         return doubleBedPrices.get(season);
-    }
+    }*/
 
 }
