f95e558d8a7702706e960efd05d866a36bd62abb
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2021-06-28 19:55:18 +0200

    16.6 JDBC - commit 1.

---
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DBManager.java
@@ -0,0 +1,32 @@
+package com.kodilla.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBManager {
+
+    private Connection connection;
+    private static DBManager dbManagerInstance;
+
+    private DBManager() throws SQLException {
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", "kodilla_user");
+        connectionProperties.put("password", "kodilla_Pass123");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" + "&useSSL=False",
+                connectionProperties);
+    }
+
+    public static DBManager getInstance() throws SQLException {
+        if (dbManagerInstance == null) {
+            dbManagerInstance = new DBManager();
+        }
+        return dbManagerInstance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
