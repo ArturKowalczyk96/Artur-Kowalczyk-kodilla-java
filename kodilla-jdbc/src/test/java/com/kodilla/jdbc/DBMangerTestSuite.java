bdb5fb960fba0656f4d70fb2acd51f38d03dfd74
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2021-07-01 17:47:10 +0200

    16.6 JDBC - commit 3 - zadanie.

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DBMangerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DBMangerTestSuite.java
@@ -3,9 +3,14 @@ package com.kodilla.jdbc;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.stream.Stream;
 
 public class DBMangerTestSuite {
 
@@ -40,5 +45,32 @@ public class DBMangerTestSuite {
         rs.close();
         statement.close();
         assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException, IOException {
+        //Given
+        DBManager dbManager = DBManager.getInstance();
+        //FileReader fileReader = new FileReader();
+
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_COUNT\n" +
+                "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID\n" +
+                "GROUP BY P.USER_ID\n" +
+                "HAVING COUNT(*) > 1;";
+        //String sqlQuery = fileReader.readFile();
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(1, counter);
     }
 }
